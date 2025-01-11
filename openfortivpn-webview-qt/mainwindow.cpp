4018e3b0229175cf5395dbf4f722f3d478902a9a
Gabriele Mazzotta <g.mazzotta@localhost>
2025-01-11 21:16:13 +0100

    qt: Fix a null pointer exception
    This line got lost when applying the changes to support Qt 6.4.

--- openfortivpn-webview-qt/mainwindow.cpp
+++ openfortivpn-webview-qt/mainwindow.cpp
@@ -21,6 +21,7 @@ MainWindow::MainWindow(const bool keepOpen,
                        QWidget *parent) :
     QMainWindow(parent),
     webEngineProfile(new QWebEngineProfile("vpn", parent)),
+    webEnginePage(new QWebEnginePage(webEngineProfile)),
 #if QT_VERSION >= QT_VERSION_CHECK(6, 4, 0)
     webEngine(new QWebEngineView(webEngineProfile, parent)),
 #else
